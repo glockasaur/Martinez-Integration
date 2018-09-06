f5cc55d1dd2e7e3c128eaa06f5badce915b4c51b
Odalys Martinez-Villa <omartinezvilla7769@eagle.fgcu.edu>
2018-09-06 09:44:09 -0400

    Initial commit

---
+++ src/mainn.java
@@ -0,0 +1,9 @@
+// Odalys Martinez-Villa
+// a program that shows off my skills
+public class mainn {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, welcome to my program");
+	}
+
+}
